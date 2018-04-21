d3114ca04ccc417b8a2945eab57925aa403b55c6
cloudera <cloudera@quickstart.cloudera>
2018-04-20 19:43:21 -0700

    mapreduce program polarity per tweet

---
+++ MapReduce/src/TwitterPolarity.java
@@ -0,0 +1,133 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class TwitterPolarity {
+	
+	private static final Map<String, Double> dictionary = new HashMap<String, Double>();
+	
+
+	public static void readToHashMap() throws NumberFormatException, IOException{
+		BufferedReader in =  new BufferedReader(new FileReader("../data/all/dictionary.v4.txt"));
+		String line = "";
+		line = in.readLine();
+		while((line = in.readLine()) != null){
+			String parts[] = line.split("\t");
+			dictionary.put(parts[0], Double.parseDouble(parts[1]));
+		}
+		in.close();
+	}
+
+  public static class PolarityMapper extends MapReduceBase
+      implements Mapper<LongWritable, Text, Text, MapWritable> {
+
+	private final static String REGEX = "([a-zA-Z]+)";
+	private final static MapWritable map = new MapWritable();
+    
+    public void map(LongWritable key, Text val,
+        OutputCollector<Text, MapWritable> output, Reporter reporter)
+        throws IOException {
+
+     
+      Pattern p = Pattern.compile(REGEX);
+      String line = val.toString();
+      String words[] = line.split(" ");
+      
+      for(int i = 0; i < words.length; i++){
+    	  Matcher m = p.matcher(words[i]);
+    	  if(m.find()){
+    		  if(dictionary.containsKey(m.group(0))){
+    			  Text k = new Text(m.group(0));
+    			  if(!map.containsKey(k))
+    				  map.put(k, new DoubleWritable(dictionary.get(m.group(0))));
+    			  
+    		  }
+    	  }
+      }
+      
+      
+      output.collect(new Text(line), map);
+    }
+  }
+
+
+
+  public static class PolarityReducer extends MapReduceBase
+      implements Reducer<Text, MapWritable, Text, DoubleWritable> {
+
+	  //reducing function - add up values for each key to get count of conferences per city
+    public void reduce(Text key, Iterator<MapWritable> values,
+        OutputCollector<Text, DoubleWritable> output, Reporter reporter)
+        throws IOException {
+
+    	double avg = 0.0;
+    	double sum = 0.0;
+    	if(values.hasNext()){
+    		MapWritable m = values.next();
+    		Set<Writable> words = m.keySet();
+    		for(Writable word : words){
+    			DoubleWritable score = (DoubleWritable) m.get(word);
+    			sum += score.get();
+    		}
+    		avg = sum/words.size();
+    	}
+    	
+    	output.collect(key,  new DoubleWritable(avg));
+    }
+  }
+
+
+  /**
+   * The actual main() method for our program; this is the
+   * "driver" for the MapReduce job.
+   */
+  public static void main(String[] args) throws Exception {
+	  readToHashMap(); 
+	  System.out.println(dictionary.toString());
+	  System.out.println(dictionary.size());
+    JobConf conf = new JobConf(TwitterPolarity.class);
+    conf.setJobName("MapReduce");
+    
+    conf.setMapOutputKeyClass(Text.class);
+    conf.setMapOutputValueClass(MapWritable.class);
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(DoubleWritable.class);
+    
+    conf.setMapperClass(PolarityMapper.class);
+    conf.setReducerClass(PolarityReducer.class);
+    
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+    
+    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+    FileOutputFormat.setOutputPath(conf,  new Path(args[1]));
+    
+    JobClient.runJob(conf);
+    
+  }
+}
